5b8cf255a958d7899d773131925e0ac3fbff061a
UMRGRS <qxrvynilp@gmail.com>
2023-11-03 10:35:24 -0600

    SP script

---
+++ Databasescripts/procesosIntegradora.sql
@@ -0,0 +1,36 @@
+DELIMITER $$
+drop procedure if exists SelectInfo $$
+create procedure SelectInfo(tableToConsult varchar(50), idToConsult int)
+begin
+	declare Error_num int;
+    declare Error_Text varchar(50);
+	declare continue handler for sqlexception
+		begin
+			set Error_num = -1;
+            set Error_Text = "Ocurrio un error";
+        end;
+		if (tableToConsult is null or idToConsult is null) then
+			set Error_num = -1;
+            set Error_Text = "Ocurrio un error";
+		else
+			select * from tableToConsult where ID = idToConsult;
+        end if;
+end $$
+DELIMITER ;
+
+DELIMITER $$
+drop procedure if exists SelectCompatibility $$
+create procedure SelectCompatibility(table1 varchar(20), table2 varchar(20), IdToConsult int)
+begin
+	declare Error_num int;
+    declare Error_Text varchar(50);
+    declare tableToConsult varchar (50);
+	declare continue handler for sqlexception
+		begin
+			set Error_num = -1;
+            set Error_Text = "Ocurrio un error";
+        end;
+	set tableToConsult = concat("compatibilidad_",table1"_",table2);
+	select * from tableToConsult where ID = IdToConsult;
+end $$
+DELIMITER ;
