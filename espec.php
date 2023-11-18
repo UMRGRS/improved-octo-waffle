d5084ca317c2034568061af6e959c5deb55a58d0
carmain93 <aabraham93@hotmail.com>
2023-11-18 15:38:20 -0600

    correcion

--- espec.php
+++ espec.php
@@ -62,12 +62,119 @@
       </p>
     </div>
 
+<<<<<<< HEAD
     <div class="card-body">
       <a href="#" class="card-link">link</a>
+=======
+<div class="container-fluid mt-4 mb-4">
+    <div class="card">
+      <div class="row g-0">
+        <div class="d-flex col-3 justify-content-center align-self-center">
+          <img src="assets/images/defec.jpeg" class="img rounded" style="height: 400px; width:400px;">
+        </div>
+        <div class="col-9">
+          <div class="card-body">
+            <h5 class="card-title fs-1">Placa madre</h5>
+            <ul class="list-group list-group-flush">
+              <li class="list-group-item fs-5">Modelo</li>
+              <li class="list-group-item fs-5">**Modelo**</li>
+              <li class="list-group-item fs-5">Chipset</li>
+              <li class="list-group-item fs-5">**Chipset**</li>
+              <li class="list-group-item fs-5">Socket</li>
+              <li class="list-group-item fs-5">**Socket**</li>
+              <li class="list-group-item fs-5">DDR</li>
+              <li class="list-group-item fs-5">**DDR**</li>
+              <li class="list-group-item fs-5">Slots RAM</li>
+              <li class="list-group-item fs-5">**Slots RAM**</li>
+              <li class="list-group-item fs-5">Wifi</li>
+              <li class="list-group-item fs-5">**Wifi**</li>
+              <li class="list-group-item fs-5">Bluetooth</li>
+              <li class="list-group-item fs-5">**Bluetooth**</li>
+              <li class="list-group-item fs-5">Slots nvme</li>
+              <li class="list-group-item fs-5">**Slots nvme**</li>
+              <li class="list-group-item fs-5">Consumo</li>
+              <li class="list-group-item fs-5">**Consumo**</li>
+            </ul>
+            <a href="#" class="btn btn-primary mt-3">Link</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="container-fluid mt-4 mb-4">
+    <div class="card">
+      <div class="row g-0">
+        <div class="d-flex col-3 justify-content-center align-self-center">
+          <img src="assets/images/defec.jpeg" class="img rounded" style="height: 400px; width:400px;">
+        </div>
+        <div class="col-9">
+          <div class="card-body">
+            <h5 class="card-title fs-1">Procesador</h5>
+            <ul class="list-group list-group-flush">
+              <li class="list-group-item fs-5">Modelo</li>
+              <li class="list-group-item fs-5">**Modelo**</li>
+              <li class="list-group-item fs-5">Nucleos</li>
+              <li class="list-group-item fs-5">**nucleos**</li>
+              <li class="list-group-item fs-5">Socket</li>
+              <li class="list-group-item fs-5">**Socket**</li>
+              <li class="list-group-item fs-5">Igpu</li>
+              <li class="list-group-item fs-5">**igpu**</li>
+              <li class="list-group-item fs-5">Disipador</li>
+              <li class="list-group-item fs-5">**disipador**</li>
+              <li class="list-group-item fs-5">Consumo</li>
+              <li class="list-group-item fs-5">**Consumo**</li>
+            </ul>
+            <a href="#" class="btn btn-primary mt-3">Link</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="container-fluid mt-4 mb-4">
+    <div class="card">
+      <div class="row g-0">
+        <div class="d-flex col-3 justify-content-center align-self-center">
+          <img src="assets/images/defec.jpeg" class="img rounded" style="height: 400px; width:400px;">
+        </div>
+        <div class="col-9">
+          <div class="card-body">
+            <h5 class="card-title fs-1">Targeta grafica</h5>
+            <ul class="list-group list-group-flush">
+              <li class="list-group-item fs-5">Modelo</li>
+              <li class="list-group-item fs-5">**Modelo**</li>
+              <li class="list-group-item fs-5">Marca</li>
+              <li class="list-group-item fs-5">**marca**</li>
+              <li class="list-group-item fs-5">Ensamblador</li>
+              <li class="list-group-item fs-5">**ensamblador**</li>
+              <li class="list-group-item fs-5">Vram</li>
+              <li class="list-group-item fs-5">**vram**</li>
+              <li class="list-group-item fs-5">Gddr</li>
+              <li class="list-group-item fs-5">**gddr**</li>
+              <li class="list-group-item fs-5">Displayport</li>
+              <li class="list-group-item fs-5">**displayport**</li>
+              <li class="list-group-item fs-5">Hdmi</li>
+              <li class="list-group-item fs-5">**hdmi**</li>
+              <li class="list-group-item fs-5">Slots</li>
+              <li class="list-group-item fs-5">**slots**</li>
+              <li class="list-group-item fs-5">Consumo</li>
+              <li class="list-group-item fs-5">**Consumo**</li>
+              <li class="list-group-item fs-5">Longitud</li>
+              <li class="list-group-item fs-5">**longitud**</li>
+            </ul>
+            <a href="#" class="btn btn-primary mt-3">Link</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+>>>>>>> 68a7ec8 (correcion)
 
     </div>
   </div>
 
+<<<<<<< HEAD
   <div class="card" style="width: 18rem;">
     <img src="assets/images/defec.jpeg" class="card-img-top" alt="...">
     <div class="card-body">
@@ -163,6 +270,166 @@
   <?php
   include("footer.html");
   ?>
+=======
+  <div class="container-fluid mt-4 mb-4">
+    <div class="card">
+      <div class="row g-0">
+        <div class="d-flex col-3 justify-content-center align-self-center">
+          <img src="assets/images/defec.jpeg" class="img rounded" style="height: 400px; width:400px;">
+        </div>
+        <div class="col-9">
+          <div class="card-body">
+            <h5 class="card-title fs-1">Ram</h5>
+            <ul class="list-group list-group-flush">
+              <li class="list-group-item fs-5">Modelo</li>
+              <li class="list-group-item fs-5">**Modelo**</li>
+              <li class="list-group-item fs-5">Marca</li>
+              <li class="list-group-item fs-5">**Marca**</li>
+              <li class="list-group-item fs-5">Tipo_ddr</li>
+              <li class="list-group-item fs-5">**tipo_ddr**</li>
+              <li class="list-group-item fs-5">Frecuencia</li>
+              <li class="list-group-item fs-5">**frecuencia**</li>
+              <li class="list-group-item fs-5">Consumo</li>
+              <li class="list-group-item fs-5">**Consumo**</li>
+            </ul>
+            <a href="#" class="btn btn-primary mt-3">Link</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="container-fluid mt-4 mb-4">
+    <div class="card">
+      <div class="row g-0">
+        <div class="d-flex col-3 justify-content-center align-self-center">
+          <img src="assets/images/defec.jpeg" class="img rounded" style="height: 400px; width:400px;">
+        </div>
+        <div class="col-9">
+          <div class="card-body">
+            <h5 class="card-title fs-1">Gabinete</h5>
+            <ul class="list-group list-group-flush">
+              <li class="list-group-item fs-5">Modelo</li>
+              <li class="list-group-item fs-5">**Modelo**</li>
+              <li class="list-group-item fs-5">Marca</li>
+              <li class="list-group-item fs-5">**Marca**</li>
+              <li class="list-group-item fs-5">Dimensiones</li>
+              <li class="list-group-item fs-5">**dimensiones**</li>
+              <li class="list-group-item fs-5">Forma</li>
+              <li class="list-group-item fs-5">**factor_de_forma**</li>
+              <li class="list-group-item fs-5">Ventana</li>
+              <li class="list-group-item fs-5">**ventana**</li>
+            </ul>
+            <a href="#" class="btn btn-primary mt-3">Link</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="container-fluid mt-4 mb-4">
+    <div class="card">
+      <div class="row g-0">
+        <div class="d-flex col-3 justify-content-center align-self-center">
+          <img src="assets/images/defec.jpeg" class="img rounded" style="height: 400px; width:400px;">
+        </div>
+        <div class="col-9">
+          <div class="card-body">
+            <h5 class="card-title fs-1">SSD</h5>
+            <ul class="list-group list-group-flush">
+              <li class="list-group-item fs-5">Modelo</li>
+              <li class="list-group-item fs-5">**Modelo**</li>
+              <li class="list-group-item fs-5">Marca</li>
+              <li class="list-group-item fs-5">**Marca**</li>
+              <li class="list-group-item fs-5">Capacidad</li>
+              <li class="list-group-item fs-5">**capacidad**</li>
+              <li class="list-group-item fs-5">Velocidad</li>
+              <li class="list-group-item fs-5">**velocidad**</li>
+              <li class="list-group-item fs-5">Consumo</li>
+              <li class="list-group-item fs-5">**consumo**</li>
+            </ul>
+            <a href="#" class="btn btn-primary mt-3">Link</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="container-fluid mt-4 mb-4">
+    <div class="card">
+      <div class="row g-0">
+        <div class="d-flex col-3 justify-content-center align-self-center">
+          <img src="assets/images/defec.jpeg" class="img rounded" style="height: 400px; width:400px;">
+        </div>
+        <div class="col-9">
+          <div class="card-body">
+            <h5 class="card-title fs-1">Ventilador</h5>
+            <ul class="list-group list-group-flush">
+              <li class="list-group-item fs-5">Marca</li>
+              <li class="list-group-item fs-5">**Marca**</li>
+              <li class="list-group-item fs-5">Medida</li>
+              <li class="list-group-item fs-5">**Medida**</li>
+              <li class="list-group-item fs-5">Consumo</li>
+              <li class="list-group-item fs-5">**consumo**</li>
+            </ul>
+            <a href="#" class="btn btn-primary mt-3">Link</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="container-fluid mt-4 mb-4">
+    <div class="card">
+      <div class="row g-0">
+        <div class="d-flex col-3 justify-content-center align-self-center">
+          <img src="assets/images/defec.jpeg" class="img rounded" style="height: 400px; width:400px;">
+        </div>
+        <div class="col-9">
+          <div class="card-body">
+            <h5 class="card-title fs-1">Dicipador</h5>
+            <ul class="list-group list-group-flush">
+              <li class="list-group-item fs-5">Modelo</li>
+              <li class="list-group-item fs-5">**modelo**</li>
+              <li class="list-group-item fs-5">Socket</li>
+              <li class="list-group-item fs-5">**socket**</li>
+            </ul>
+            <a href="#" class="btn btn-primary mt-3">Link</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  
+  <div class="container-fluid mt-4 mb-4">
+    <div class="card">
+      <div class="row g-0">
+        <div class="d-flex col-3 justify-content-center align-self-center">
+          <img src="assets/images/defec.jpeg" class="img rounded" style="height: 400px; width:400px;">
+        </div>
+        <div class="col-9">
+          <div class="card-body">
+            <h5 class="card-title fs-1">fuente</h5>
+            <ul class="list-group list-group-flush">
+              <li class="list-group-item fs-5">Modelo</li>
+              <li class="list-group-item fs-5">**Modelo**</li>
+              <li class="list-group-item fs-5">Marca</li>
+              <li class="list-group-item fs-5">**Marca**</li>
+              <li class="list-group-item fs-5">Potencia</li>
+              <li class="list-group-item fs-5">**potencia**</li>
+              <li class="list-group-item fs-5">Certificacion</li>
+              <li class="list-group-item fs-5">**certificacion*</li>
+            </ul>
+            <a href="#" class="btn btn-primary mt-3">Link</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+
+
+>>>>>>> 68a7ec8 (correcion)
 </body>
 
 </html>
